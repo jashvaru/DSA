b1265c7f35b3b6dee6d919322411d79fed708b37
jashvaru <jashvaru@gmail.com>
2025-01-05 19:37:51 +0530

    Creating a java file

---
+++ JavaCode/src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Patterns patterns = new Patterns();
+        patterns.pattern8(5);
+    }
+}
+
+
