1394f8a618eedee10877242489d9f6850e2942d5
jashvaru <jashvaru@gmail.com>
2025-01-12 20:01:00 +0530

    Leetcode solution for Armstrong

--- 2-BasicMath/Leetcode/ArmstrongNumber/Armstorng.java
+++ 2-BasicMath/Leetcode/ArmstrongNumber/Armstorng.java
@@ -8,14 +8,27 @@ public class Armstorng {
 
         while(x > 0) {
             ld = x % 10;
-            System.out.println("last digit: " + ld);
-            System.out.println("last digit cube: " + ld * ld * ld);
             arm = arm + ld * ld * ld;
-            System.out.println("arm: " + arm);
             x = x / 10;
-            System.out.println("x: " + x);
         }
 
         System.out.println(og == arm ? true : false);
     }
 }
+
+/*
+-We know how to reverse input using basic math that is extract digit using modulus of 10 and 
+add that digit into the reference number(initially 0) by multiplying it by 10.
+To reduce input by 1 digit divide by 10.
+-As we have each digit cube it and do a summation at each iteration
+
+
+Time Complexity
+O(log10(n)) - because while loop will run till input is 0 and modulus can be 
+represented using log10 as we are dividing the number with 10. (if divide by 2 it would be log2(n))
+
+Space Complexity
+O(1) - As we are using constant number of variable irrespective of input
+ */
+
+//Time taken to solve = 5 mins
