48a0f9011d0cce0a41122efdd10471fc61e92b09
jashvaru <jashvaru@gmail.com>
2025-01-12 19:32:35 +0530

    Leetcode solution for Palindrome

--- 2-BasicMath/Leetcode/Palindrome/Palindrome.java
+++ 2-BasicMath/Leetcode/Palindrome/Palindrome.java
@@ -16,3 +16,20 @@ public class Palindrome {
         System.out.println(og < 0 ? false : og == reverse ? true : false); 
     }
 }
+
+/*
+-We know how to reverse input using basic math that is extract digit using modulus of 10 and 
+add that digit into the reference number(initially 0) by multiplying it by 10.
+To reduce input by 1 digit divide by 10.
+-As we want to reverse both positive and negative value while loop will stop when input is 0
+
+
+Time Complexity
+O(log10(n)) - because while loop will run till input is 0 and modulus can be 
+represented using log10 as we are dividing the number with 10. (if divide by 2 it would be log2(n))
+
+Space Complexity
+O(1) - As we are using constant number of variable irrespective of input
+ */
+
+//Time taken to solve = 5 mins
