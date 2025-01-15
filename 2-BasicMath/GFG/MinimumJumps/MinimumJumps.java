ab3cf7f87c28f8c3aa6dcf732601aa189d8bda27
jashvaru <jashvaru@gmail.com>
2025-01-15 08:55:08 +0530

    Attempt to solve minimum jump problem statement

--- 2-BasicMath/GFG/MinimumJumps/MinimumJumps.java
+++ 2-BasicMath/GFG/MinimumJumps/MinimumJumps.java
@@ -1,13 +1,25 @@
 public class MinimumJumps {
 
     public static int minJumps(int[] arr) {
-        int minJumps = 1;
+        int minJumps = 0;
+        int i = 0;
+        while(i < arr.length-1) {
+            if(arr[i] == 0) {
+                if(minJumps == 0) {
+                    minJumps = -1;
+                }
+                break;
+            } else {
+                i = i + arr[i];
+                minJumps++;
+            }
+        }
 
         return minJumps;
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,1};
+        int[] arr = {9,10,1,2,3,4,8,0,0,0,0,0,0,0,1};
         System.out.println(minJumps(arr));
     }
 }
