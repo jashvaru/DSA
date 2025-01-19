46fbf45a7ad0b64388e8c4525208a7425d217d81
jashvaru <jashvaru@gmail.com>
2025-01-19 11:38:26 +0530

    GFG solution to reverse self array

--- 3-Recursion/GFG/ReverseArray/ReverseArray.java
+++ 3-Recursion/GFG/ReverseArray/ReverseArray.java
@@ -28,3 +28,46 @@ public class ReverseArray {
         
     }
 }
+
+
+/* 
+--Basic solution
+public class ReverseArray {
+    
+    static void reverseArray(int[] arr) {
+        int end = arr.length - 1;
+        int start = 0;
+        
+        while(start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {21, 30, 45};
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        System.out.println();
+        reverseArray(arr);
+        System.out.println();
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+        
+    }
+}
+
+Time complexity
+O(n/2) - as while loop will run till half of the array
+
+space complexity
+O(1) - as we are using only temp var in each loop
+ */
+
+
