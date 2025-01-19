756a758ad1f9a4c801a5ac3336de671aea107edd
jashvaru <jashvaru@gmail.com>
2025-01-19 11:51:46 +0530

    GFG solution to reverse self array

--- 3-Recursion/GFG/ReverseArray/ReverseArray.java
+++ 3-Recursion/GFG/ReverseArray/ReverseArray.java
@@ -3,15 +3,20 @@ public class ReverseArray {
     static void reverseArray(int[] arr) {
         int end = arr.length - 1;
         int start = 0;
-        
-        while(start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
+        fetch(start, end, arr);
+    }
 
+
+    static void fetch(int start, int end, int[] arr) {
+        if(start > end) {
+            return;
+        }
+        start++;
+        end--;
+        fetch(start, end, arr);
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 
     public static void main(String[] args) {
@@ -28,6 +33,20 @@ public class ReverseArray {
         
     }
 }
+
+/*
+Recursion
+-we only need to iterate through the array till half of it.
+-As seen in below how this will work
+[2,6,7,4] -swap(0,3)-> [4,6,7,2] -swap(1,2)-> [4,7,6,2]
+
+Time complexity
+O(n) - as base consition will be true at half of the array
+
+Space complexity
+O(n) - As each new method call will occupy new space in stack
+ */
+
 
 
 /* 
@@ -64,7 +83,8 @@ public class ReverseArray {
 }
 
 Time complexity
-O(n/2) - as while loop will run till half of the array
+Actual - O(n/2)
+Not considering (1/2) - O(n) as while loop will run till half of the array
 
 space complexity
 O(1) - as we are using only temp var in each loop
