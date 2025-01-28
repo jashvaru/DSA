47b1201f0b1763b4791b5941b2fce6bb74d1de7b
jashvaru <jashvaru@gmail.com>
2025-01-28 08:39:08 +0530

    Code for Selection Sort

---
+++ 5-Sorting/SortingAlgo/SelectionSort/SelectionSort.java
@@ -0,0 +1,22 @@
+public class SelectionSort {
+ 
+    public static void selectionSort(int[] arr) {
+        for(int i = 0; i <= arr.length-2; i++) {
+            for(int j = i+1; j <= arr.length-1; j++) {
+                if(arr[j] < arr[i]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,3,5,2,6,1};
+        selectionSort(arr);
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
