dfdf0dfe8f666531089336c8df584737df7f2cfd
jashvaru <jashvaru@gmail.com>
2025-02-05 08:57:52 +0530

    GFG solution for Second Lagest element in array (Better approach)

--- 6-Arrays/Easy/GFG/SecondLargestElementInArray/SecondLargestElementInArray.java
+++ 6-Arrays/Easy/GFG/SecondLargestElementInArray/SecondLargestElementInArray.java
@@ -92,7 +92,6 @@
         }
 
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + " " + secondLargest + " " + largest);
             if (arr[i] > secondLargest && arr[i] != largest) {
                 secondLargest = arr[i];
             }
