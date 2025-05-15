f7a7a4c98802bc5f0ea5dfa61f7af3689f978ad1
jashvaru <jashvaru@gmail.com>
2025-05-15 08:26:14 +0530

    Leetcode solution for count the max number of consecutive 1's

---
+++ 6-Arrays/Easy/Leetcode/MaxConsecutiveOnes/MaxConsecutiveOnes.java
@@ -0,0 +1,42 @@
+import java.lang.Math.*;
+
+public class MaxConsecutiveOnes {
+
+    private int findMaxConsecutiveOnes(int[] nums) {
+        int cCounter = 0;
+        int counter = 0;
+        for (int n : nums) {
+            if(n == 1) {
+                counter++;
+            } else {
+                cCounter = Math.max(cCounter, counter);
+                counter = 0;
+            }
+        }
+        cCounter = Math.max(cCounter, counter);
+        return cCounter;
+    } 
+    
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1};
+        MaxConsecutiveOnes max = new MaxConsecutiveOnes();
+        int c = max.findMaxConsecutiveOnes(nums);
+        System.out.println("Max Cosecutive ones = " + c);
+    }
+}
+
+/*
+ * Logic
+ * -Using a variable to count the consecutive 1's and other to keep track of max_count value
+ * -Count will be reset if we come across 0, when this happens we compare count and max_count and get
+ * max of those two.
+ * -One loops get completed we again compare those two and get tha max value so at the end we have max value  
+ *
+ * Time Complexity
+ * It takes
+ * O(n) - we iterate through the loop once
+ *
+ * Space Complexity
+ * O(1) - as we are not using any new space
+ * */
---
+++ 6-Arrays/Easy/Leetcode/MaxConsecutiveOnes/README.md
@@ -0,0 +1,28 @@
+<h1>Max Consecutive Ones</h1>
+
+<p> Link to problem statement - <a href="https://leetcode.com/problems/max-consecutive-ones/description/">Max Consecutive Ones</a></p>
+
+<p>Given a binary array <code>nums</code>, return the maximum number of consecutive <code>1</code>'s in the array.</p>
+
+<h2>Example 1:</h2>
+
+<pre>
+<strong>Input:</strong> nums = [1,1,0,1,1,1]
+<strong>Output:</strong> 3
+<strong>Explanation:</strong> The first two digits or the last three digits are consecutive 1s. 
+The maximum number of consecutive 1s is 3.
+</pre>
+
+<h2>Example 2:</h2>
+
+<pre>
+<strong>Input:</strong> nums = [1,0,1,1,0,1]
+<strong>Output:</strong> 2
+</pre>
+
+<h2>Constraints:</h2>
+
+<ul>
+  <li>1 <= nums.length <= 10<sup>5</sup></li>
+  <li><code>nums[i]</code> is either <code>0</code> or <code>1</code>.</li>
+</ul>
